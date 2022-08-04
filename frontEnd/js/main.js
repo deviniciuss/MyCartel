3bfb4ee935af8d96015fff332b569ee93d38e4e1
deviniciuss <vinicius_rosa98@outlook.com>
2022-08-04 19:43:42 +0100

    map finished and database connections maybe done

--- frontEnd/js/main.js
+++ frontEnd/js/main.js
@@ -1,40 +1,35 @@
 $(document).ready(function () {
 
-    initMap();
+
      
 });
-
-const cartels = [
-    {
-        position: {lat:206597,lng:-1033496},
-        map: map,
-    },
-    {
-        position: {lat:325149,lng:-1170382},     
-        map: map,
-    },
-    {
-        position: {lat:196058,lng:-990365},
-        map: map,
-    }
-];
-
-function addMarker(props){
-
-    var marker = new google.maps.Marker({
-
-        position: props.position,
-        map: props.map
-    })
-} 
 
 function initMap(){
 
     var options = {
         zoom: 6,
-        center:{lat:23.6345,lng:-102.5528}
+        center:{lat:23.6100,lng:-102.5528}
     }
 
     var map = new google.maps.Map(document.getElementById('map'), options);
-}
+   
+    var marker = new google.maps.Marker({
+
+        position: {lat:20.6597,lng:-103.3496},
+        map: map
+    });
+
+    var marker = new google.maps.Marker({
+
+        position: {lat:24.2669,lng:-98.8363},
+        map: map
+    });
+
+    var marker = new google.maps.Marker({
+
+        position: {lat:19.6058,lng:-99.0365},
+        map: map
+    });
+};
+
 
